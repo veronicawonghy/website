ba208a9c0b41814d61a3b3ceb059fd72b3ca7bf4
Veronica <veronicawonghy@gmail.com>
2022-01-09 20:40:31 +0800

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "../logo.png"
+
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"/></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About Me</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+              {/* <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li> */}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+}
+
+export default Navbar;
